5b35771269739f5a1d6943d43f7c94e72b348c45
seok-ju <choi87066@gmail.com>
2019-08-26 20:57:50 +0900

    test파일 삭제 / 기본 java파일 생성

---
+++ src/main/java/com/conn/spring/DAO/ProductDao.java
@@ -0,0 +1,12 @@
+package com.conn.spring.DAO;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductDao {
+	
+	private SqlSessionTemplate sqlSession;
+	
+	
+}
---
+++ src/main/java/com/conn/spring/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.conn.spring.service;
+
+import com.conn.spring.DAO.ProductDao;
+
+public class ProductService {
+	
+	private ProductDao productDao;
+}
